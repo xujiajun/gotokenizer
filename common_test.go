7aea30170ad3f8e0e97bdb50f926f7cec46caf9e
xujiajun <me@xujiajun.cn>
2018-10-15 11:47:44 +0800

    rename package name

--- common_test.go
+++ common_test.go
@@ -1,4 +1,4 @@
-package go_tokenizer
+package gotokenizer
 
 var (
 	dictZhPath                = "data/zh/dict.txt"
--- maximum_matching_test.go
+++ maximum_matching_test.go
@@ -1,4 +1,4 @@
-package go_tokenizer
+package gotokenizer
 
 import (
 	"reflect"
